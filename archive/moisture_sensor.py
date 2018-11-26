42ee1e5cf9dddc371567305b41bc3c743d9ed098
lucio <seanluciotolentino@gmail.com>
2018-11-26 09:42:18 -0500

    initial commit

---
+++ archive/moisture_sensor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import PCF8591 as ADC
+import RPi.GPIO as GPIO
+import time
+import math
+import sys
+#sys.stdout = open('data/sensor_data.csv', 'w')
+
+
+#DO = 17
+#GPIO.setmode(GPIO.BCM)
+
+def setup():
+	ADC.setup(0x48)
+	#GPIO.setup(DO, GPIO.IN)
+
+def Print(x):
+	if x == 1:
+		print ''
+		print '   ***************'
+		print '   * Not raining *'
+		print '   ***************'
+		print ''
+	if x == 0:
+		print ''
+		print '   *************'
+		print '   * Raining!! *'
+		print '   *************'
+		print ''
+
+def loop():
+	while True:
+            print time.strftime('%Y-%m-%d %H:%M:%S'),',',ADC.read(0)
+            sys.stdout.flush()
+            time.sleep(30)
+
+if __name__ == '__main__':
+	try:
+		setup()
+		loop()
+	except KeyboardInterrupt: 
+		pass	
---
+++ sensors.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import PCF8591 as ADC
+import time
+import math
+import sys
+
+TEMP_PIN = 0
+LIGHT_PIN = 2
+MOISTURE_PIN = 1
+SLEEP_LENGTH = 5*60  # 5 minutes?
+
+def get_temperature():
+	# there's math on top of the analog read so make it a function
+	analogVal = ADC.read(TEMP_PIN)
+	Vr = 5 * float(analogVal) / 255
+	Rt = 10000 * Vr / (5 - Vr)
+	temp = 1/(((math.log(Rt / 10000)) / 3950) + (1 / (273.15+25)))
+	return temp - 273.15
+
+
+def loop():
+	while True:
+		# collect the data
+		temp = get_temperature()  # there's some math with this
+		light = ADC.read(LIGHT_PIN)
+		moisture = ADC.read(MOISTURE_PIN)
+		timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+
+		# write the data
+		print ','.join([str(v) for v in [timestamp,temp, light, moisture]])
+		sys.stdout.flush()
+
+		time.sleep(SLEEP_LENGTH)
+
+if __name__ == '__main__':
+	try:
+		ADC.setup(0x48)  # I'm not sure exactly what this does but seems necessary
+		loop()
+	except KeyboardInterrupt: 
+		pass
