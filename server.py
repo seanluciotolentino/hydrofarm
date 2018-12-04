8f3e7e412f5ec456dcfdde94010d91ec93d44416
Sean Tolentino <stolentino@dotdash.com>
2018-12-04 16:31:02 -0500

    add function to save images

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 from flask import Flask, request
+from werkzeug.utils import secure_filename
+import os
+
+#UPLOAD_FOLDER = '/home/lucio/Desktop/hydroimages'
+UPLOAD_FOLDER = '/Users/stolentino/Desktop/hydroimages'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
@@ -19,7 +25,18 @@ def measure():
 		print("Server can't handle those parameters")
 		return 'fail', 400
 	
+@app.route('/record/', methods=['POST'])
+def record():
+	print('record called')
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		print('no file supplied')
+		return 'fail', 400
+	file = request.files['file']
+	if file and file.filename.endswith('.png'):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return 'sucess', 200
 
 if __name__ == '__main__':   
-	
 	app.run(debug=True)
